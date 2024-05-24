bdcecfaddac35c74dbaf891e3e9aa9ccdd3b5cdf
Carlos M. Rodriguez Satanana <carrodsan10@alu.edu.gva.es>
2024-05-24 22:43:49 +0200

    Añadido componente lista de contactos

--- src/panels/MainPanel.java
+++ src/panels/MainPanel.java
@@ -1,23 +1,26 @@
 package panels;
 
+import components.ContactsList;
 import components.HeaderMain;
 import components.Search;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class MainPanel extends JPanel {
     private HeaderMain header;
     private Search search;
+    private ContactsList contactsList;
 
     public MainPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setBackground(Color.WHITE);
+        setBackground(null);
 
         header = new HeaderMain();
         search = new Search("Buscar Contacto...");
+        contactsList = new ContactsList();
 
         add(header);
         add(search);
+        add(contactsList);
     }
 }
